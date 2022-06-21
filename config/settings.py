52e88426b15b414549e3f84f1ddbdad9cc7c4c7a
asliddin2002developer <eshonkulovasliddindevloper@gmail.com>
2022-06-21 13:21:23 +0500

    whatever you want

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.heroku.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'asldevblog.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
